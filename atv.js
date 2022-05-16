c3038b470ee89e7d001cd0f044b1fe2b5561c886
miguelarcanjo1 <102737532+miguelarcanjo1@users.noreply.github.com>
2022-05-16 10:41:33 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,30 @@
+//atv 1
+
+//let x =1 //<- contador
+//while(x <= 100){
+    //console.log(x)
+   // x = x + 1
+//}
+
+//atv 2
+
+//let x =50 //<- contador
+//while(x <= 100){
+   // console.log(x)
+   // x = x + 1
+//}
+
+//atv3
+
+//let x =10 //<- contador
+//while(x >= 0){
+    //console.log(x)
+    //x = x - 1
+//}
+
+let x =0 //<- contador
+while(x <= 20){
+    console.log(x)
+    x = x + 2
+}
+
---
+++ laços.js
@@ -0,0 +1,37 @@
+
+//let x = 1
+//while (x <=10){
+    //console.log('olá, tudo bem?')
+   // x = x + 1
+//}
+
+//faça um laço que repita o seu nome 3 vezes
+
+//let x =0 //<- contador
+//while(x < 2){
+    //console.log('Miguel')
+    //x = x + 1
+//}
+
+//faça um laço que mostre os valores do contador.
+
+//let x =0 //<- contador
+//while(x < 3){
+    //console.log(x)
+   // x = x + 1
+//}
+
+//faça um laço que mostre os valores pares 0 á 10
+//let x =1 //<- contador
+//while(x <= 10){
+   // console.log(x)
+   // x = x + 2
+//}
+
+
+let x =3 //<- contador
+while(x <= 10){
+    console.log(x)
+    x = x + 3
+}
+
